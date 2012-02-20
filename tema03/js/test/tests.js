bf3957c42ff1b7ed481b5d7fb42eceaee609378a
Alex Ciminian <alex.ciminian@gmail.com>
2012-02-21 01:06:23 +0200

    refactoring tests

--- tema03/js/test/tests.js
+++ tema03/js/test/tests.js
@@ -5,7 +5,7 @@
 
     // hacking all HTMLElements' prototype to enable event checking
     // Kids, don't try this at home! Or better yet, anywhere!
-    if (typeof HTMLElement === 'function') {
+    if (typeof HTMLElement === 'function' || typeof HTMLElement === 'object') {
         HTMLElement.prototype.registeredEvents = [];
         HTMLElement.prototype.decoratedAddEventListener = HTMLElement.prototype.addEventListener;
         HTMLElement.prototype.addEventListener = function (eventType, action, capturingPhase) {
@@ -30,26 +30,102 @@
         }
         
         // if we got here, need to check if the event was added via the attribute
-        return !!el['on'+eventType];
+        return !!el['on' + eventType];
     };
 
-	function initFormFactory() {
-		var styles = {
-				formClass:     'formStyle',
-				inputClass:    'inputStyle',
-				comboClass:    'comboStyle',
-				submitClass:   'submitStyle',
-				containerType: 'div',
-				containerClass: 'containerStyle'
-		}, formFactory = createFormFactory(styles);
-		return formFactory;
-	}
-	
+    var getConfigObject = function (type) {
+        switch (type) {
+        case 'input':
+            return {
+                name : randomString(),
+                id : randomString(),
+                type : 'text',
+                value : randomString(5)
+            };
+        break;
+        case 'submit':
+            return {
+                name : randomString(),
+                id : randomString(),
+                type : 'submit',
+                value : randomString(5)
+            }
+        break;
+        case 'select':
+            return {
+                name : randomString(),
+                id : randomString(),
+                type : 'select',
+                value : [randomString(3), randomString(6)],
+                description : randomString(7)
+            }
+        break;
+        case 'form':
+            return {
+                name : randomString(),
+                id : randomString(),
+                method : 'GET',
+                action : randomString() + '.php',
+                elements : [getConfigObject('input'), getConfigObject('dropdown'), getConfigObject('input'), getConfigObject('submit')]
+            }
+        break;
+        case 'factory':
+            return {
+                styles : {
+                    form : randomString(),
+                    input : {
+                        text : randomString(),
+                        submit : randomString()
+                    },
+                    select : randomString(),
+                },
+                container : {
+                    type : randomValue(['div', 'p']),
+                    style : ['container', randomString()]
+                };
+            }
+        default:
+        break;
+        }
+    };
+
+    var randomString = function (length) {
+        var alphabet = 'abcdefghijklmnopqrstuvwxyz', i, result = '';
+        for (i = 0; i < (length || 4); i++) {
+            result += randomValue(alphabet);
+        }
+    };
+
+    var randomValue = function (values) {
+        return values[Math.floor(Math.random() * values.length)];
+    };
+
+    var checkCommon = function (el, config, attributes) {
+        var attrs = attributes || ['id', 'value', 'name'];
+        ok(el.nodeType === 1, 'The HTML element has a correct node type');
+        $.each(attr, function (idx, attr) {
+            if (typeof config[attr] === 'string') {
+                // assert that the configuration attribute is mirrored in the element
+                ok(el.getAttribute(attr) === config[attr], 'Attribute ' + attr + ' was correctly attached.');
+            }
+        }
+    };
+
+    var checkStyles = function (el, styles) {
+        if (typeof styles === 'string') {
+            ok($(el).hasClass(style), 'CSS class ' + style + 'was correctly attached to the element');
+        } else {
+            // assumed array
+            $.each(styles, function (idx, style) {
+                checkStyles(el, style);
+            }
+        }
+    };
+
     module('Preliminary Checks');
 	test('Check if required functions are present', function () {
 		equals(typeof(createFormFactory), 'function', 'createFormFactory function present');
-		var formFactory = createFormFactory();
-		// TODO: see how to test object type for private constructor
+		var formFactory = createFormFactory(getConfigObject('factory'));
 		equals(typeof(formFactory.createForm), 'function', 'createForm function present in factory object');
 		equals(typeof(formFactory.createInput), 'function', 'createInput function present in factory object');
 		equals(typeof(formFactory.createCombo), 'function', 'createCombo function present in factory object');
@@ -58,124 +134,74 @@
 	
 	
 	module('Functional checks');
-	
 	test('Check for different types of form elements', function () {
-		var formFactory = initFormFactory();
+		var formFactory = createFormFactory(getConfigObject('factory')), input, dropdown, submit, form;
 		
 		// test createInput
-		var test_input = { name:'some_name', type:'text', value:'value_1', description:'Description text:' };
-		var form_input = formFactory.createInput(test_input);
-		ok(form_input instanceof HTMLElement, 'createInput returns a DOM element');
-		equals(form_input.nodeType, 1, 'Input has correct nodeType');
+		input = formFactory.createInput(getConfigObject('input'));
+		ok(input instanceof HTMLElement, 'createInput returns a DOM element');
+		equals(input.nodeType, 1, 'Input has correct nodeType');
 		
 		// test createCombo
-		var test_combo = {name:'myCombo', id: 'myCombo', type:'combo', value: ['cv_1','cv_2','cv_3'], description:'Description text:'};
-		var form_combo = formFactory.createCombo(test_combo);
-		ok(form_combo instanceof HTMLElement, 'createCombo returns a DOM element');
+	    dropdown = formFactory.createSelect(getConfigObject('select'));
+		ok(combo instanceof HTMLElement, 'createCombo returns a DOM element');
 		
 		// test createSubmit
-		var test_submit = {name: 'submit'};
-		var form_submit = formFactory.createSubmit(test_submit);
-		ok(form_submit instanceof HTMLElement, 'createSubmit returns a DOM element');
-		equals(form_submit.nodeType, 1, 'Submit has correct nodeType');
+		submit = formFactory.createSubmit(getConfigObject('submit'));
+		ok(submit instanceof HTMLElement, 'createSubmit returns a DOM element');
+		equals(submit.nodeType, 1, 'Submit has correct nodeType');
 		
-		var test_form = {
-			name: 'formName',
-			method: 'GET',
-			action: 'script.php',
-			elements: [
-			   {name:'name_1', type:'text', value:'value_1', description:'Description text:'},
-			   {name:'name_2', type:'text', value:'value_2', description:'Description text:'},
-			   {name:'name_3', type:'text', value:'value_3', description:'Description text:'},
-			   {name:'ajaxInput',  id:'ajaxInput',  type:'text', description:'Description text:'},
-			   {name:'comboInput', id:'comboInput', type:'text', description:'Description text:'},
-			   {name:'someCombo',  id:'someCombo',  type:'combo', description:'Description text:'},
-			   {name:'mySubmit', id:'mySubmit', type:'submit', value:'trimite', description:'Description text:'}
-			]
-		};
-		var form = formFactory.createForm(test_form);
+		form = formFactory.createForm(getConfigObject('form'));
 		ok(form instanceof HTMLElement, 'createForm returns a DOM element');
 		equals(form.nodeType, 1, 'Form has correct nodeType');
 	});
 
 
-	test('Input element checks', function () {
-		var formFactory = initFormFactory();
+	test('Input Element Checks', function () {
+		var formFactory = createFormFactory(getConfigObject('factory')), config = getConfigObject('input'), input;
 		expect(5);
-		var test_input = {name:'some_name', id: 'test_input', type:'text', value:'value_1', description:'Description text:'};
-		var form_input = formFactory.createInput(test_input);
-		equals(form_input.nodeType, 1, 'Input has correct nodeType');
-		equals(form_input.getAttribute('name'), 'some_name', 'Name attribute created successfully');
-		equals(form_input.value, 'value_1', 'Value attribute created successfully');
-		equals(form_input.type, 'text', 'Type attribute created successfully');		
-		equals(form_input.id, 'test_input', 'Id attribute created successfully');
+		input = formFactory.createInput(config);
+        checkCommon(input, config);
+		equals(input.type, 'text', 'Type attribute created successfully');		
 		// TODO: test if exception is thrown when required fields are not present?
 	});
 	
-	test('Combo element checks', function () {
-		var i;
-		var formFactory = initFormFactory();
-		var test_combo = {name:'myCombo', id: 'myCombo', type:'combo', value: ['cv_1','cv_2','cv_3'], description:'Description text:'};
-		var form_combo = formFactory.createCombo(test_combo);
-		equals(form_combo.nodeType, 1, 'Combo has correct nodeType');
-		equals(form_combo.getAttribute('name'), 'myCombo', 'Name attribute created successfully');
-		equals(form_combo.id, 'myCombo', 'Id attribute created successfully');
-		equals(form_combo.childNodes.length, test_combo.value.length, 'Correct number of values');
-		for (i = 0; i < form_combo.childNodes.length; i++) {
-			equals(form_combo.childNodes[i].tagName.toLowerCase(), 'option', 'Option element was created');
-			equals(form_combo.childNodes[i].innerHTML, test_combo.value[i], 'Value was correctly inserted');
+	test('Dropdown Element Checks', function () {
+		var formFactory = createFormFactory(getConfigObject('factory')), config = getConfigObject('select'), select, i;
+		select = formFactory.createCombo(config);
+        checkCommon(select, config);
+		equals(select.childNodes.length, config.value.length, 'Correct number of values');
+		for (i = select.childNodes.length - 1; i >= 0; i--) {
+			equals(select.childNodes[i].tagName.toLowerCase(), 'option', 'Option element was created');
+			equals(select.childNodes[i].innerHTML, config.value[i], 'Value was correctly inserted');
 		}		
 	});
 	
 	test('Submit element checks', function () {
-		var formFactory = initFormFactory();
-		var test_submit = {name: 'mySubmit', id: 'mySubmit', value: 'Send', description:'Description text:'};
-		var form_submit = formFactory.createSubmit(test_submit);
-		equals(form_submit.nodeType, 1, 'Combo has correct nodeType');
-		equals(form_submit.getAttribute('name'), 'mySubmit', 'Name attribute created successfully');
-		equals(form_submit.value, 'Send', 'Value created successfully');
-		equals(form_submit.type, 'submit', 'Type attribute created successfully');		
-		equals(form_submit.id, 'mySubmit', 'Id attribute created successfully');
+		var formFactory = createFormFactory(getConfigObject('factory')), config = getConfigObject('submit'), submit;
+		submit = formFactory.createSubmit(config);
+        checkCommon(submit, config);
+		equals(submit.type, 'submit', 'Type attribute created successfully');		
 		
 	});
 	
 	test('Test complete form', function () {
 		var i;
-		var formFactory = initFormFactory();
-		var form_factory_styles = {
-				formClass:     'formStyle',
-				inputClass:    'inputStyle',
-				comboClass:    'comboStyle',
-				submitClass:   'submitStyle',
-				containerType: 'div',
-				containerClass: 'containerStyle'
-		};
-		
-		var form_info = {
-			name: 'formName',
-			method: 'GET',
-			action: 'script.php',
-			elements: [
-			   {name:'name_1', type:'text', value:'value_1', description: 'Input label'},
-			   {name:'myCombo', id: 'myCombo',   type:'combo', value: ['cv_1','cv_2','cv_3'], description: 'Combo label'},
-			   {name:'mySubmit', id:'mySubmit', type:'submit', value:'trimite', description: 'Submit label'}
-			]
-		};
-		
-		var form = formFactory.createForm(form_info);
-		equals(form.nodeType, 1, 'Form has correct nodeType');
-		equals(form.getAttribute('action'), form_info.action, 'Form has correct action');
-		equals(form.getAttribute('method'), form_info.method, 'Form has correct method');
+		var factoryConfig = getConfigObject('factory'), 
+            factory = createFormFactory(factoryConfig), 
+            formConfig = getConfigObject('form'),
+            form = factory.createForm(formConfig);
+	
+        checkCommon(form, formConfig, ['action', 'method']);
+		equals(form.className, factoryConfig.styles.form);
 		equals(form.childNodes.length, 3, 'Form has correct number of child nodes');
-		equals(form.className, form_factory_styles.formClass);
 		for (i = 0; i < form.childNodes.length; i++) {
-			equals(form.childNodes[i].className, form_factory_styles.containerClass);
-			equals(form.childNodes[i].tagName.toLowerCase(), form_factory_styles.containerType.toLowerCase());
+			equals(form.childNodes[i].className, factoryConfig.container.style.join(' ')); // TODO: make this work in all cases
+			equals(form.childNodes[i].tagName.toLowerCase(), factoryConfig.container.type.toLowerCase());
 		}
-		equals($(form).find('input[type=text]').attr('class'), form_factory_styles.inputClass, 'Input class was correctly added.');
-		equals($(form).find('select').attr('class'), form_factory_styles.comboClass, 'Combo class was correctly added.');
-		equals($(form).find('input[type=submit]').attr('class'), form_factory_styles.submitClass, 'Submit class was correctly added.');
-		console.log($(form));
+		checkStyles($(form).find('input[type=text]').get(0), factoryConfig.styles.input);
+		checkStyles($(form).find('select').get(0), factoryConfig.styles.select);
+		checkStyles($(form).find('input[type=submit]').get(0), factoryConfig.styles.submit);
 		equals($(form).find('div > label').length, 3, 'Labels were inserted correctly for every element.');
 		equals($(form).find('div > label').get(0).innerHTML, 'Input label', 'Input label was inserted correctly');
 		equals($(form).find('div > label').get(1).innerHTML, 'Combo label', 'Combo label was inserted correctly');
@@ -184,7 +210,7 @@
 	});
 	
 	asyncTest('AJAX Tests', function () {
-		var formFactory = initFormFactory();
+		var formFactory = createFormFactory(getConfigObject('factory'));
 		// ajax metadata
 		var ajax_info = { url: AJAX_SCRIPT_URL, event: 'change', target: 'myCombo' };
 		var ajax_info_2 = { url: AJAX_SCRIPT_URL, event: 'click',	target: 'mySubmit' };
